5f789f0f072fae8e0c82c90bbd1284d41d5bd02a
Mert Çolakoğlu <xmert45@gmail.com>
2024-07-07 01:34:37 +0300

    feat: use ready template

--- profilepro/settings.py
+++ profilepro/settings.py
@@ -127,6 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATİC_ROOT = BASE_DIR / 'static'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
