219a6ff8efb7c2122023cf2c437507edaa6ada1e
Mert Çolakoğlu <xmert45@gmail.com>
2024-07-07 14:08:57 +0300

    feat: add general setting model to admin

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from mainapp.models import *
 
 # Register your models here.
+
+
+@admin.register(GeneralSetting)
+class GeneralSettingsAdmin(admin.ModelAdmin):
+
+    list_display = ['id', 'name', 'description', 'parameters', 'updated_date', 'created_date']
+    search_fields = ['name', 'description', 'parameters', 'updated_date', 'created_date']
+    list_editable = ('description', 'parameters')
+
+    class Meta:
+        model = GeneralSetting
--- mainapp/models.py
+++ mainapp/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class GeneralSettings(models.Model):
+
+class GeneralSetting(models.Model):
     name = models.CharField(default='', max_length=100, blank=True, verbose_name='Name', help_text='Name',)
-    description = models.TextField(default='', max_length=254, blank=True, verbose_name='Description', help_text='Description',)
-    parameters = models.CharField(default='',max_length=254, blank=True, verbose_name='Parameters', help_text='Parameters',)
+    description = models.CharField(default='', max_length=254, blank=True, verbose_name='Description', help_text='Description',)
+    parameters = models.TextField(default='',max_length=254, blank=True, verbose_name='Parameters', help_text='Parameters',)
     updated_date = models.DateTimeField(blank=True, auto_now=True, verbose_name='Updated Date', help_text='Updated Date',)
     created_date = models.DateTimeField(blank=True, auto_now_add=True, verbose_name='Created Date', help_text='Created Date',)
 
