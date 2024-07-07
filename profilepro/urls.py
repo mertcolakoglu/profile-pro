815210892a8f8711ef49980669335601963d6683
Mert Çolakoğlu <xmert45@gmail.com>
2024-07-07 12:56:26 +0300

    feat: create contact app

--- profilepro/urls.py
+++ profilepro/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainapp.urls')),
+    path('', include('contact.urls')),
 ]
 
 if settings.DEBUG:
