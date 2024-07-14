2d4b6eaa659b8702e24cd405428749df5e870db6
Mert Çolakoğlu <xmert45@gmail.com>
2024-07-14 17:13:24 +0300

    feat: create contact application

---
+++ contact/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from mainapp.models import AbstractBaseModel
+
+# Create your models here.
+
+
+class Message(AbstractBaseModel):
+    name = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+        verbose_name='Name',
+        help_text='',
+    )
+    email = models.EmailField(
+        default='',
+        max_length=254,
+        blank=True,
+        verbose_name='Email',
+        help_text='',
+    )
+    subject = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+        verbose_name='Subject',
+        help_text='',
+    )
+    message = models.TextField(
+        default='',
+        blank=True,
+        verbose_name='Message',
+        help_text='',
+    )
+
+    def __str__(self):
+        return f'Message: {self.name}'
+
+    class Meta:
+        verbose_name = 'Message'
+        verbose_name_plural = 'Messages'
+        ordering = ('name',)
